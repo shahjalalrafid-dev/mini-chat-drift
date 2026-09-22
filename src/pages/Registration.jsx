6398c7d57070c9c84e80c49328e6d41985e0579c
shahjalalrafid-dev <shahjalalrafid201694@gmail.com>
2026-09-22 12:09:03 +0600

    Registration Page Completed Responsively

--- src/pages/Registration.jsx
+++ src/pages/Registration.jsx
@@ -5,16 +5,23 @@ import RegistrationImage from '../assets/registration.png'
 
 const Registration = () => {
   return (
-    <div className='container mx-auto'>
-        <div className='flex justify-center'>
-            <div>
-                <h1 className='font-nunito font-bold text-4xl text-[#11175D]'>Get started with easily register</h1>
-                <h4 className='font-nunito font-normal text-xl text-[#000000] opacity-50 mt-4 mb-10'>Free register and you can enjoy it</h4>
-                <TextField className='block! mb-8!' id="outlined-basic" label="Outlined" variant="outlined" />
-                <TextField className='block! mb-8!' id="outlined-basic" label="Outlined" variant="outlined" />
-                <TextField className='block! mb-8!' id="outlined-basic" label="Outlined" variant="outlined" />
-                <Button variant="contained">Sign Up</Button>
-                <p>Already have an account ? <span>Sign In</span> </p>
+    <div className='container mx-auto px-7'>
+        <div className='flex justify-center gap-17 lg:flex-row flex-col'>
+            <div className='my-auto'>
+                <h1 className='font-nunito font-bold text-4xl text-[#11175D] text-center lg:text-left'>Get started with easily register</h1>
+                <h4 className='font-nunito font-normal text-xl text-[#000000] opacity-50 mt-4 mb-10 text-center lg:text-left'>Free register and you can enjoy it</h4>
+                <div className='w-[90%] lg:w-[70%]'>
+                    <TextField className=' mb-8! w-full' id="outlined-basic" label="Email Address" variant="outlined" />
+                </div>
+
+                <div className='w-[90%] lg:w-[70%]'>
+                <TextField className=' mb-8! w-full' id="outlined-basic" label="Password" variant="outlined" />
+                </div>
+                <div className='w-[90%] lg:w-[70%]'>
+                <TextField className=' w-full mb-8!' id="outlined-basic" label="Full Name" variant="outlined" />
+                </div>
+                <Button variant="contained" className='px-[150px]! py-5! rounded-full! '>Sign Up</Button>
+                <p className='mt-10 text-left ml-15'>Already have an account ? <span className='text-orange-400 font-bold cursor-pointer' >Sign In</span> </p>
             </div>
             <div>
                 <img className='w-full h-screen object-cover' src= {RegistrationImage} alt="Registration Image" />

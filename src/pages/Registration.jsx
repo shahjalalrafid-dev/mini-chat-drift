4a6fdc3a53deb035e92d06ced9b23cd3c7cbcf81
shahjalalrafid-dev <shahjalalrafid201694@gmail.com>
2026-09-22 16:24:47 +0600

    Finally Completed

--- src/pages/Registration.jsx
+++ src/pages/Registration.jsx
@@ -10,21 +10,21 @@ const Registration = () => {
             <div className='my-auto'>
                 <h1 className='font-nunito font-bold text-4xl text-[#11175D] text-center lg:text-left'>Get started with easily register</h1>
                 <h4 className='font-nunito font-normal text-xl text-[#000000] opacity-50 mt-4 mb-10 text-center lg:text-left'>Free register and you can enjoy it</h4>
-                <div className='w-[90%] lg:w-[70%]'>
+                <div className='w-[90%] lg:w-[90%]'>
                     <TextField className=' mb-8! w-full' id="outlined-basic" label="Email Address" variant="outlined" />
                 </div>
 
-                <div className='w-[90%] lg:w-[70%]'>
+                <div className='w-[90%] lg:w-[90%]'>
                 <TextField className=' mb-8! w-full' id="outlined-basic" label="Password" variant="outlined" />
                 </div>
-                <div className='w-[90%] lg:w-[70%]'>
+                <div className='w-[90%] lg:w-[90%]'>
                 <TextField className=' w-full mb-8!' id="outlined-basic" label="Full Name" variant="outlined" />
                 </div>
                 
-                <Button variant="contained" className='w-[70%]! py-5! ml-15! lg:ml-0! rounded-full!'>Sign Up</Button>
+                <Button variant="contained" className='w-[90%]! py-5! rounded-full!'>Sign Up</Button>
                 
                 
-                <p className='mt-10 text-left ml-17 '>Already have an account ? <span className='text-orange-400 font-bold cursor-pointer' >Sign In</span> </p>
+                <p className='mt-10 text-center '>Already have an account ? <span className='text-orange-400 font-bold cursor-pointer' >Sign In</span> </p>
             </div>
             <div>
                 <img className='w-full h-screen object-cover' src= {RegistrationImage} alt="Registration Image" />
